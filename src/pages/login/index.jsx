b0b0389b61f732809be79b525f582894bcfc7c9c
Josué Alves <jow17alves@gmail.com>
2021-12-08 11:57:09 -0300

    feat: added useForm, yupresolver and yup libraries

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,3 +1,16 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
 export const Login = () => {
-  return <h1>Login</h1>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data) => console.log(data);
+  return <form></form>;
 };
