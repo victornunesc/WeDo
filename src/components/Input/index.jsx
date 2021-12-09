6ff0109558abb6fe0a5bef12878e6ead08000dc7
Marcelo Silveira <marcelosj3@hotmail.com>
2021-12-08 21:58:06 -0300

    feat: add radio input component
    Created an input radio container to contain the radio inputs;
    Created an input radio;
    Styled both components to fit the mockup.

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 
 import { FiEye, FiEyeOff } from 'react-icons/fi';
 
-import { Container, ContainerPassword } from './style';
+import { Container, ContainerPassword, ContainerRadio } from './style';
 
 export const Input = ({ placeholder, register, name, errors }) => {
   const [haveText, setHaveText] = useState(false);
@@ -23,6 +23,31 @@ export const Input = ({ placeholder, register, name, errors }) => {
       <p className="placeholder">{placeholder}</p>
       {isErrored && <p className="error">{errors[name]?.message}</p>}
     </Container>
+  );
+};
+
+export const InputRadioContainer = ({ title, name, errors, children }) => {
+  const [isErrored, setIsErrored] = useState(false);
+
+  useEffect(() => {
+    setIsErrored(!!errors[name]?.message);
+  }, [errors[name]?.message]);
+
+  return (
+    <ContainerRadio isErrored={isErrored}>
+      <h3 className="title">{title}</h3>
+      <section className="options">{children}</section>
+      {isErrored && <p className="error">{errors[name]?.message}</p>}
+    </ContainerRadio>
+  );
+};
+
+export const InputRadio = ({ register, name, label, sizeBigger }) => {
+  return (
+    <ContainerRadio className="options__option" sizeBigger={sizeBigger}>
+      <input type="radio" id={label} {...register(name)} />
+      <label htmlFor={label}>{label}</label>
+    </ContainerRadio>
   );
 };
 
